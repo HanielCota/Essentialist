a34d7394cf4ae6f1431adb9e980f86861caad199
Haniel Cota <hanielcota@hotmail.com>
2026-05-28 21:30:36 -0300

    refactor(menu): extract shared MenuViewCleanupListener
    Replace the near-identical InfoMenuCleanupListener and ModulesMenuCleanupListener
    with one reusable MenuViewCleanupListener in core menu/, parameterised by menuId
    and an onClear(UUID) callback. Removes the duplicated close/quit plumbing
    (SonarCloud new-code duplication) and keeps it written once.

--- src/main/java/com/hanielcota/essentials/modules/essentials/EssentialsModule.java
+++ src/main/java/com/hanielcota/essentials/modules/essentials/EssentialsModule.java
@@ -2,13 +2,13 @@ package com.hanielcota.essentials.modules.essentials;
 
 import com.github.hanielcota.menuframework.api.MenuService;
 import com.hanielcota.essentials.config.ConfigService;
+import com.hanielcota.essentials.menu.MenuViewCleanupListener;
 import com.hanielcota.essentials.module.AbstractModule;
 import com.hanielcota.essentials.module.control.ModuleControl;
 import com.hanielcota.essentials.module.environment.ModuleEnvironment;
 import com.hanielcota.essentials.module.registration.ModuleRegistrar;
 import com.hanielcota.essentials.modules.essentials.command.EssentialsCommand;
 import com.hanielcota.essentials.modules.essentials.config.EssentialsConfig;
-import com.hanielcota.essentials.modules.essentials.listener.ModulesMenuCleanupListener;
 import com.hanielcota.essentials.modules.essentials.menu.EssentialsModulesMenu;
 import com.hanielcota.essentials.modules.essentials.menu.EssentialsModulesMenuState;
 import com.hanielcota.essentials.modules.essentials.menu.ModulesMenuRenderer;
@@ -32,7 +32,8 @@ public final class EssentialsModule extends AbstractModule {
     var menu = new EssentialsModulesMenu(config, state, renderer, control);
     registrar.menu(menu);
 
-    var cleanupListener = new ModulesMenuCleanupListener(state, menus);
+    var cleanupListener =
+        new MenuViewCleanupListener(menus, EssentialsModulesMenu.ID, state::clear);
     registrar.listener(cleanupListener);
 
     var essentialsCommand = new EssentialsCommand(config, configs, menus);
